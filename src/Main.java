95ada9449e10e0490c6405934a447c9f0ef826e5
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-11-27 12:47:14 +0530

    UC4 Played till player reaches the winning position

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,40 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game!");
 
-        int Start_Position = 0;
-        System.out.println("Starting Position of Player: "+Start_Position);
+        int position = 0;
+        int Winning_Postion = 100;
+        System.out.println("Starting Position of Player: " + position);
 
         Random random = new Random();
-        int dice = random.nextInt(6)+1;
-        System.out.println("Rolled the die: "+dice);
+        while (position < Winning_Postion) {
+            int dice = random.nextInt(6) + 1;
+            System.out.println("Rolled the die: " + dice);
 
+            int option = random.nextInt(3);
+            switch (option) {
+                case 0:
+                    System.out.println("Option: No Play. Player stays at position: " + position);
+                    break;
+                case 1:
+                    position += dice;
+                    if (position > Winning_Postion) {
+                        position -= dice;
+                    } else {
+                        System.out.println("Option: Ladder. Player climbs to: " + position);
+                    }
+                    break;
+                case 2:
+                    position -= dice;
+                    if (position <= 0) {
+                        position = 0;
+                    }
+                    System.out.println("Option: Snake. Player slides to: " + position);
+                    break;
+                default:
+                    System.out.println("Option is not valid");
+            }
+
+        }
+        System.out.println("Current Position " + position);
     }
-
 }
