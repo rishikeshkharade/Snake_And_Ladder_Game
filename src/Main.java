4262cfd81b74623bccabfb5db3d03f5d71bfc8e2
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-11-27 10:06:09 +0530

    UC1 Starting Position of Player

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game!");
+
+        int Start_Position = 0;
+        System.out.println("Starting Position of Player: "+Start_Position);
     }
 }
