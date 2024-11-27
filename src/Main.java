30b451b6c12505fe2bfec4ed05faa6803ebe1aef
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-11-27 11:28:05 +0530

    UC3 Checking an Options for Ladder,Snake and No play

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,33 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game!");
 
-        int Start_Position = 0;
-        System.out.println("Starting Position of Player: "+Start_Position);
+        int position = 0;
+        System.out.println("Starting Position of Player: "+position);
 
         Random random = new Random();
         int dice = random.nextInt(6)+1;
         System.out.println("Rolled the die: "+dice);
 
+        int option = random.nextInt(3);
+        switch(option){
+            case 0:
+                System.out.println("Option: No Play. Player stays at position: "+position);
+                break;
+            case 1:
+                position += dice;
+                System.out.println("Option: Ladder. Player climbs to: "+position);
+                break;
+            case 2:
+                position -= dice;
+                if (position <= 0){
+                    position=0;
+                }
+                System.out.println("Option: Snake. Player slides to: "+position);
+        break;
+
+            default:
+                System.out.println("Option is not valid");
+        }
     }
 
 }
