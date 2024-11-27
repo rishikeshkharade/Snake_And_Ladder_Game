414f6b50e721e078f5be9e3f75f791be10cffc61
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-11-27 10:29:44 +0530

    UC2 Rolled the dice

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game!");
 
         int Start_Position = 0;
         System.out.println("Starting Position of Player: "+Start_Position);
+
+        Random random = new Random();
+        int dice = random.nextInt(6)+1;
+        System.out.println("Rolled the die: "+dice);
+
     }
+
 }
